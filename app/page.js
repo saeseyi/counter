5469d929c54d1e3529cf49434e2a7b977b33b3c8
saeseyi <oluwasaejoseph@gmail.com>
2024-09-22 06:32:39 +0100

    yysaj

--- app/page.js
+++ app/page.js
@@ -5,11 +5,10 @@ export default function page() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
 
       <main className="text-center">
-        <h1 className="text-4xl font-bold mb-5">Next.js Counter Website</h1>
+        <h1 className="text-4xl font-bold mb-8 mt-5">Next.js Counter Website</h1>
         <Counter />
       </main>
-
-      <footer className="mt-5 text-center text-gray-500">
+      <footer className="mt-1 text-center text-gray-500">
         <p>Created with Next.js and Tailwind CSS</p>
       </footer>
     </div>
